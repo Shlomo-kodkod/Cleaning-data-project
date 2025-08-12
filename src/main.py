8bdf0710d789b274c6b1ee7a3ec5fb321a3a0842
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-08-12 11:14:51 +0300

    docs: del path

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from manager import Manager
 
 
 if __name__ == "__main__":
-    manager = Manager(r"C:\Users\user\VsCodeProjects\Python\Cleaning data project\data\tweets_dataset.csv", 'Biased')
+    manager = Manager(r"data\tweets_dataset.csv", 'Biased')
     manager.main()
